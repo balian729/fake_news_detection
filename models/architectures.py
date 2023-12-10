c30f3bc3940d0a252370355e58502c0c692b734b
Dmitrii <15dnau@gmail.com>
2023-12-11 02:05:15 +0300

    add the gcnfn model

---
+++ models/architectures.py
@@ -0,0 +1,60 @@
+from torch.nn import Linear
+from torch_geometric.nn import global_mean_pool, GATConv
+import torch
+import torch.nn.functional as F
+
+
+class GCNFN(torch.nn.Module):
+	"""
+GCNFN is implemented using two GCN layers and one mean-pooling layer as the graph encoder; 
+the 310-dimensional node feature (args.feature = content) is composed of 300-dimensional 
+comment word2vec (spaCy) embeddings plus 10-dimensional profile features 
+
+Paper: Fake News Detection on Social Media using Geometric Deep Learning
+Link: https://arxiv.org/pdf/1902.06673.pdf
+
+
+Model Configurations:
+
+Vanilla GCNFN: args.concat = False, args.feature = content
+UPFD-GCNFN: args.concat = True, args.feature = spacy
+"""
+
+	def __init__(self, args):
+		super(GCNFN, self).__init__()
+
+		self.num_features = args.num_features
+		self.num_classes = args.num_classes
+		self.nhid = args.nhid
+		self.concat = args.concat
+
+		self.conv1 = GATConv(self.num_features, self.nhid * 2)
+		self.conv2 = GATConv(self.nhid * 2, self.nhid * 2)
+
+		self.fc1 = Linear(self.nhid * 2, self.nhid)
+
+		if self.concat:
+			self.fc0 = Linear(self.num_features, self.nhid)
+			self.fc1 = Linear(self.nhid * 2, self.nhid)
+
+		self.fc2 = Linear(self.nhid, self.num_classes)
+
+
+	def forward(self, data):
+		x, edge_index, batch = data.x, data.edge_index, data.batch
+
+		x = F.selu(self.conv1(x, edge_index))
+		x = F.selu(self.conv2(x, edge_index))
+		x = F.selu(global_mean_pool(x, batch))
+		x = F.selu(self.fc1(x))
+		x = F.dropout(x, p=0.5, training=self.training)
+
+		if self.concat:
+			news = torch.stack([data.x[(data.batch == idx).nonzero().squeeze()[0]] for idx in range(data.num_graphs)])
+			news = F.relu(self.fc0(news))
+			x = torch.cat([x, news], dim=1)
+			x = F.relu(self.fc1(x))
+
+		x = F.log_softmax(self.fc2(x), dim=-1)
+
+		return x
---
+++ models/gcnfn.py
@@ -0,0 +1,132 @@
+import argparse
+import sys
+from tqdm import trange
+import torch.nn.functional as F
+from torch.utils.data import random_split
+from torch_geometric.data import DataLoader
+from torch.nn import Linear
+from torch_geometric.nn import global_mean_pool, GATConv
+
+from os.path import dirname
+sys.path.append(dirname(dirname(__file__)))
+
+from utils.dataloader import *
+from utils.graphic_utils import plot_figure
+from utils.eval import eval_deep, compute_test
+from models.architectures import GCNFN
+
+def train(model, train_loader, val_loader, args):
+	optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+	model.train()
+
+	loop = trange(args.epochs)
+
+	train_loss_list = []
+	val_loss_list = []
+
+	train_acc_list = []
+	val_acc_list = []
+
+	train_auc_list = []
+	val_auc_list = []
+
+	for _ in loop:
+		out_log = []
+		loss_train = 0.0
+		for i, data in enumerate(train_loader):
+			optimizer.zero_grad()
+			data = data.to(args.device)
+			out = model(data)
+			y = data.y
+			loss = F.nll_loss(out, y)
+			loss.backward()
+			optimizer.step()
+			loss_train += loss.item()
+			out_log.append([F.softmax(out, dim=1), y])
+		acc_train, _, _, _, recall_train, auc_train, _ = eval_deep(out_log, train_loader)
+		[acc_val, _, _, _, recall_val, auc_val, _], loss_val = compute_test(model, val_loader, args.device)
+		loop.set_postfix(
+			loss_train=loss_train,
+			acc_train=acc_train,
+			loss_val=loss_val,
+			acc_val=acc_val
+		)
+		train_loss_list.append(loss_train)
+		val_loss_list.append(loss_val)
+
+		train_acc_list.append(acc_train)
+		val_acc_list.append(acc_val)
+
+		train_auc_list.append(auc_train)
+		val_auc_list.append(auc_val)
+	plot_figure(y_label="Loss", y1=train_loss_list, y2=val_loss_list, epochs=args.epochs)
+	plot_figure(
+			 y1=train_acc_list, 
+			 y2=val_acc_list,
+			 y_label="Accuracy",
+			 y1_label="Train accuracy", 
+			 y2_label="Val accuracy", 
+			 file_name="Train_val_acc.png",
+			 epochs=args.epochs)
+	plot_figure(
+			 y1=train_auc_list,
+			 y2=val_auc_list,
+			 y_label="AUC",
+			 y1_label="Train AUC",
+			 y2_label="Val AUC",
+			 file_name="Train_val_auc",
+			 epochs=args.epochs)
+	
+	
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+
+	# original model parameters
+	parser.add_argument('--device', type=str, default='cuda:0', help='specify cuda devices')
+
+	# hyper-parameters
+	parser.add_argument('--batch_size', type=int, default=128, help='batch size')
+	parser.add_argument('--train_size', type=float, default=0.6)
+	parser.add_argument('--val_size', type=float, default=0.2)
+	parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
+	parser.add_argument('--weight_decay', type=float, default=0.01, help='weight decay')
+	parser.add_argument('--nhid', type=int, default=128, help='hidden size')
+	parser.add_argument('--epochs', type=int, default=60, help='maximum number of epochs')
+	parser.add_argument('--concat', type=bool, default=False, help='whether concat news embedding and graph embedding')
+	parser.add_argument('--feature', type=str, default='spacy', help='feature type, [profile, spacy, bert, content]')
+
+	args = parser.parse_args()
+	SEED = 42
+	torch.manual_seed(SEED)
+	if torch.cuda.is_available():
+		torch.cuda.manual_seed(SEED)
+
+	dataset = FNNDataset(root='data', feature=args.feature, empty=False, name='politifact', transform=ToUndirected())
+
+	args.num_classes = dataset.num_classes
+	args.num_features = dataset.num_features
+
+	print(args)
+
+	num_training = int(len(dataset) * args.train_size)
+	num_val = int(len(dataset) * args.val_size)
+	num_test = len(dataset) - (num_training + num_val)
+
+	training_set, validation_set, test_set = random_split(dataset, [num_training, num_val, num_test])
+
+
+	train_loader = DataLoader(training_set, batch_size=args.batch_size, shuffle=True)
+	val_loader = DataLoader(validation_set, batch_size=args.batch_size, shuffle=False)
+	test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=False)
+
+	model = GCNFN(args=args).to(args.device)
+	model = model.to(args.device)
+
+	train(model, train_loader, val_loader, args)
+
+	# Model training
+	
+	[acc, f1_macro, f1_micro, precision, recall, auc, ap], test_loss = compute_test(model, test_loader, args.device, verbose=False)
+	print(f'Test set results: acc: {acc:.4f}, f1_macro: {f1_macro:.4f}, f1_micro: {f1_micro:.4f}')
+	print(f'precision: {precision:.4f}, recall: {recall:.4f}, auc: {auc:.4f}, ap: {ap:.4f}')
